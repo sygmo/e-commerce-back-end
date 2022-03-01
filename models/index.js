9595340538df5b6e1759590389062c2a9b18fc25
Sydney Mercier <sydney.mercier91@gmail.com>
2022-02-28 18:49:47 -0600

    implemented product routes

--- models/index.js
+++ models/index.js
@@ -6,29 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+  // onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
+  foreignKey: 'product_id',
+  through: ProductTag
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
+  foreignKey: 'tag_id',
+  through: ProductTag
 });
 
 module.exports = {
